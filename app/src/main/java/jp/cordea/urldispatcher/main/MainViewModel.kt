2f304750a9084ac5c5e428a9662a1b387f7e9595
Yoshihiro Tanaka <contact@cordea.jp>
2018-10-31 21:37:54 +0900

    add view model

---
+++ app/src/main/java/jp/cordea/urldispatcher/main/MainViewModel.kt
@@ -0,0 +1,9 @@
+package jp.cordea.urldispatcher.main
+
+import androidx.lifecycle.ViewModel
+import jp.cordea.urldispatcher.UrlRepository
+
+class MainViewModel(
+        private val repository: UrlRepository
+) : ViewModel() {
+}
