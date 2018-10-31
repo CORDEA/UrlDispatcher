38cf7b8bc13ff5e3c73a5696e6cbd539820ef991
Yoshihiro Tanaka <contact@cordea.jp>
2018-10-31 21:41:23 +0900

    add module

---
+++ app/src/main/java/jp/cordea/urldispatcher/appModule.kt
@@ -0,0 +1,18 @@
+package jp.cordea.urldispatcher
+
+import androidx.room.Room
+import jp.cordea.urldispatcher.main.MainViewModel
+import org.koin.android.ext.koin.androidContext
+import org.koin.androidx.viewmodel.ext.koin.viewModel
+import org.koin.dsl.module.module
+
+val appModule = module {
+    single {
+        Room.databaseBuilder(androidContext(), AppDatabase::class.java, "urlDispatcher").build()
+    }
+    single { get<AppDatabase>().urlDao() }
+    single { UrlLocalDataSource(get()) }
+    single<UrlRepository> { UrlRepositoryImpl(get()) }
+
+    viewModel { MainViewModel(get()) }
+}
