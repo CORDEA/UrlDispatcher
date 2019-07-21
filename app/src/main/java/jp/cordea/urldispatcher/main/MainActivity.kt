ec919cb0929dc5a050098fcd78ea8b8eca8fc9fa
Yoshihiro Tanaka <contact@cordea.jp>
2019-07-21 12:12:14 +0900

    add main fragment

--- app/src/main/java/jp/cordea/urldispatcher/main/MainActivity.kt
+++ app/src/main/java/jp/cordea/urldispatcher/main/MainActivity.kt
@@ -1,67 +1,28 @@
 package jp.cordea.urldispatcher.main
 
-import android.app.Activity
-import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
-import io.reactivex.disposables.Disposable
-import io.reactivex.rxkotlin.subscribeBy
+import androidx.navigation.Navigation.findNavController
 import jp.cordea.urldispatcher.R
-import jp.cordea.urldispatcher.add.AddActivity
 import jp.cordea.urldispatcher.databinding.ActivityMainBinding
-import kotlinx.android.synthetic.main.activity_main.*
-import org.koin.android.ext.android.inject
-import org.koin.androidx.scope.ext.android.bindScope
-import org.koin.androidx.scope.ext.android.getOrCreateScope
-import org.koin.androidx.viewmodel.ext.android.viewModel
-import org.koin.core.parameter.parametersOf
 
 class MainActivity : AppCompatActivity() {
 
-    private val viewModel: MainViewModel by viewModel()
-    private val adapter: MainAdapter by inject { parametersOf(this@MainActivity) }
-    private val navigator: MainNavigator by inject { parametersOf(this@MainActivity) }
-
-    private lateinit var binding: ActivityMainBinding
-
-    private var disposable: Disposable? = null
+    private val navController by lazy { findNavController(this, R.id.main_nav_fragment) }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
-        bindScope(getOrCreateScope(SCOPE))
-        setSupportActionBar(toolbar)
-        supportActionBar?.title = getString(R.string.title_main)
-        binding.recyclerView.adapter = adapter
+        val binding = DataBindingUtil.setContentView<ActivityMainBinding>(this, R.layout.activity_main)
+        setSupportActionBar(binding.toolbar)
 
-        binding.fab.setOnClickListener {
-            navigator.navigateToAdd()
+        navController.addOnDestinationChangedListener { _, destination, _ ->
+            val id = destination.id
+            when (id) {
+                R.id.mainFragment -> supportActionBar?.title = getString(R.string.title_main)
+            }
         }
-
-        disposable = viewModel.adapterItems
-                .subscribeBy { adapter.update(it) }
-
-        viewModel.refresh()
     }
 
-    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
-        super.onActivityResult(requestCode, resultCode, data)
-        if (resultCode != Activity.RESULT_OK) {
-            return
-        }
-        if (requestCode != AddActivity.REQUEST_CODE) {
-            return
-        }
-        viewModel.refresh()
-    }
-
-    override fun onDestroy() {
-        super.onDestroy()
-        disposable?.dispose()
-    }
-
-    companion object {
-        const val SCOPE = "MainActivity"
-    }
+    override fun onSupportNavigateUp(): Boolean = navController.navigateUp()
 }
---
+++ app/src/main/java/jp/cordea/urldispatcher/main/MainFragment.kt
@@ -0,0 +1,66 @@
+package jp.cordea.urldispatcher.main
+
+
+import android.app.Activity
+import android.content.Intent
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import io.reactivex.disposables.Disposable
+import jp.cordea.urldispatcher.add.AddActivity
+import jp.cordea.urldispatcher.databinding.FragmentMainBinding
+import org.koin.androidx.scope.ext.android.bindScope
+import org.koin.androidx.scope.ext.android.getOrCreateScope
+import org.koin.androidx.viewmodel.ext.android.viewModel
+
+class MainFragment : Fragment() {
+
+    private val viewModel: MainViewModel by viewModel()
+//    private val adapter: MainAdapter by inject { parametersOf(this@MainActivity) }
+//    private val navigator: MainNavigator by inject { parametersOf(this@MainActivity) }
+
+    private lateinit var binding: FragmentMainBinding
+
+    private var disposable: Disposable? = null
+
+    override fun onCreateView(
+            inflater: LayoutInflater,
+            container: ViewGroup?,
+            savedInstanceState: Bundle?
+    ): View {
+        binding = FragmentMainBinding.inflate(inflater, container, false)
+        bindScope(getOrCreateScope(SCOPE))
+//        binding.recyclerView.adapter = adapter
+
+        binding.fab.setOnClickListener {
+            //            navigator.navigateToAdd()
+        }
+
+//        disposable = viewModel.adapterItems
+//                .subscribeBy { adapter.update(it) }
+
+        viewModel.refresh()
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (resultCode != Activity.RESULT_OK) {
+            return
+        }
+        if (requestCode != AddActivity.REQUEST_CODE) {
+            return
+        }
+        viewModel.refresh()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        disposable?.dispose()
+    }
+
+    companion object {
+        private const val SCOPE = "MainFragment"
+    }
+}
