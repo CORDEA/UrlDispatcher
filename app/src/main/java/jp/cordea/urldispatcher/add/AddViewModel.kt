c0bc9546695ab53f327fb7c8705894d313137c6e
Yoshihiro Tanaka <contact@cordea.jp>
2019-07-21 22:31:00 +0900

    handle url

--- app/src/main/java/jp/cordea/urldispatcher/add/AddViewModel.kt
+++ app/src/main/java/jp/cordea/urldispatcher/add/AddViewModel.kt
@@ -1,8 +1,11 @@
 package jp.cordea.urldispatcher.add
 
 import androidx.lifecycle.ViewModel
+import io.reactivex.Flowable
 import io.reactivex.android.schedulers.AndroidSchedulers
-import io.reactivex.disposables.Disposable
+import io.reactivex.disposables.CompositeDisposable
+import io.reactivex.processors.BehaviorProcessor
+import io.reactivex.rxkotlin.addTo
 import io.reactivex.rxkotlin.subscribeBy
 import jp.cordea.urldispatcher.Url
 import jp.cordea.urldispatcher.UrlRepository
@@ -11,16 +14,28 @@ import java.util.*
 class AddViewModel(
         private val repository: UrlRepository
 ) : ViewModel() {
-    private var disposable: Disposable? = null
+    private val compositeDisposable = CompositeDisposable()
+
+    private val _url = BehaviorProcessor.create<Url>()
+    val url: Flowable<Url> = _url.observeOn(AndroidSchedulers.mainThread())
+
+    fun init(url: String?) {
+        url ?: return
+        repository.findUrl(url)
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribeBy { _url.onNext(it) }
+                .addTo(compositeDisposable)
+    }
 
     fun storeUrl(url: String, description: String) {
-        disposable = repository.insertUrl(Url(url, description, Date().time))
+        repository.insertUrl(Url(url, description, Date().time))
                 .observeOn(AndroidSchedulers.mainThread())
                 .subscribeBy { }
+                .addTo(compositeDisposable)
     }
 
     override fun onCleared() {
         super.onCleared()
-        disposable?.dispose()
+        compositeDisposable.clear()
     }
 }
