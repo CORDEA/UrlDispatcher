54d6d4b25f184184aef95b9543e916b21bdd2db6
Yoshihiro Tanaka <contact@cordea.jp>
2019-07-30 21:58:17 +0900

    check intent

--- app/src/androidTest/java/jp/cordea/urldispatcher/MainScenarioTest.kt
+++ app/src/androidTest/java/jp/cordea/urldispatcher/MainScenarioTest.kt
@@ -1,18 +1,34 @@
 package jp.cordea.urldispatcher
 
+import android.content.Intent
+import androidx.core.net.toUri
 import androidx.test.core.app.ActivityScenario
 import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.action.ViewActions.click
 import androidx.test.espresso.action.ViewActions.typeText
 import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.contrib.RecyclerViewActions
+import androidx.test.espresso.intent.Intents
+import androidx.test.espresso.intent.Intents.intended
+import androidx.test.espresso.intent.matcher.IntentMatchers.hasAction
+import androidx.test.espresso.intent.matcher.IntentMatchers.hasData
 import androidx.test.espresso.matcher.ViewMatchers.*
 import androidx.test.ext.junit.runners.AndroidJUnit4
+import com.xwray.groupie.ViewHolder
 import org.hamcrest.CoreMatchers.allOf
+import org.junit.After
+import org.junit.Before
 import org.junit.Test
 import org.junit.runner.RunWith
 
 @RunWith(AndroidJUnit4::class)
 class MainScenarioTest {
+
+    @Before
+    fun setUp() = Intents.init()
+
+    @After
+    fun tearDown() = Intents.release()
 
     @Test
     fun save() {
@@ -39,5 +55,10 @@ class MainScenarioTest {
         onView(withId(R.id.recycler_view))
                 .check(matches(withText(0, R.id.title, "http://example.com")))
                 .check(matches(withText(0, R.id.description, "description")))
+
+        onView(withId(R.id.recycler_view))
+                .perform(RecyclerViewActions.actionOnItemAtPosition<ViewHolder>(0, click()))
+
+        intended(allOf(hasAction(Intent.ACTION_VIEW), hasData("http://example.com".toUri())))
     }
 }
