64c2c461b0d24449d812d2fb1bb68be405bb39aa
Yoshihiro Tanaka <contact@cordea.jp>
2019-08-01 23:08:14 +0900

    enhance test

--- app/src/androidTest/java/jp/cordea/urldispatcher/MainScenarioTest.kt
+++ app/src/androidTest/java/jp/cordea/urldispatcher/MainScenarioTest.kt
@@ -4,8 +4,7 @@ import android.content.Intent
 import androidx.core.net.toUri
 import androidx.test.core.app.ActivityScenario
 import androidx.test.espresso.Espresso.onView
-import androidx.test.espresso.action.ViewActions.click
-import androidx.test.espresso.action.ViewActions.typeText
+import androidx.test.espresso.action.ViewActions.*
 import androidx.test.espresso.assertion.ViewAssertions.matches
 import androidx.test.espresso.contrib.RecyclerViewActions
 import androidx.test.espresso.intent.Intents
@@ -32,6 +31,9 @@ class MainScenarioTest {
 
     @Test
     fun save() {
+        val url = "http://example.com"
+        val description = "description"
+
         ActivityScenario.launch(MainActivity::class.java)
 
         onView(allOf(withParent(withId(R.id.toolbar)), withText(R.string.title_main)))
@@ -43,9 +45,9 @@ class MainScenarioTest {
                 .check(matches(isDisplayed()))
 
         onView(withParent(withParent(withId(R.id.url))))
-                .perform(typeText("http://example.com"))
+                .perform(typeText(url))
         onView(withParent(withParent(withId(R.id.description))))
-                .perform(typeText("description"))
+                .perform(typeText(description))
 
         onView(withId(R.id.fab)).perform(click())
 
@@ -53,12 +55,34 @@ class MainScenarioTest {
                 .check(matches(isDisplayed()))
 
         onView(withId(R.id.recycler_view))
-                .check(matches(withText(0, R.id.title, "http://example.com")))
-                .check(matches(withText(0, R.id.description, "description")))
+                .check(matches(withText(0, R.id.title, url)))
+                .check(matches(withText(0, R.id.description, description)))
+
+        onView(withId(R.id.recycler_view))
+                .perform(RecyclerViewActions.actionOnItemAtPosition<ViewHolder>(0, longClick()))
+
+        onView(withId(R.id.edit)).perform(click())
+
+        onView(allOf(withParent(withId(R.id.toolbar)), withText(R.string.title_add)))
+                .check(matches(isDisplayed()))
+
+        onView(withParent(withParent(withId(R.id.url))))
+                .check(matches(withText(url)))
+        onView(withParent(withParent(withId(R.id.description))))
+                .check(matches(withText(description)))
+
+//        onView(withParent(withParent(withId(R.id.url))))
+//                .perform(clearText())
+//                .perform(typeText("$url/2"))
+        onView(withParent(withParent(withId(R.id.description))))
+                .perform(clearText())
+                .perform(typeText("$description/2"))
+
+        onView(withId(R.id.fab)).perform(click())
 
         onView(withId(R.id.recycler_view))
                 .perform(RecyclerViewActions.actionOnItemAtPosition<ViewHolder>(0, click()))
 
-        intended(allOf(hasAction(Intent.ACTION_VIEW), hasData("http://example.com".toUri())))
+        intended(allOf(hasAction(Intent.ACTION_VIEW), hasData(url.toUri())))
     }
 }
