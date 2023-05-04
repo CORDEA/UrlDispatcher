80aeff74157c29255d76bd31a8f14236d0fa70ba
Yoshihiro Tanaka <contact@cordea.jp>
2023-05-04 14:12:17 +0900

    migrate to the latest

--- app/src/main/java/jp/cordea/urldispatcher/home/HomeAdapter.kt
+++ app/src/main/java/jp/cordea/urldispatcher/home/HomeAdapter.kt
@@ -1,11 +1,11 @@
 package jp.cordea.urldispatcher.home
 
 import com.xwray.groupie.GroupAdapter
-import com.xwray.groupie.ViewHolder
+import com.xwray.groupie.GroupieViewHolder
 
 class HomeAdapter(
         private val itemProvider: HomeListItemProvider
-) : GroupAdapter<ViewHolder>() {
+) : GroupAdapter<GroupieViewHolder>() {
     fun update(models: List<HomeListItemModel>) {
         clear()
         addAll(models.map { itemProvider.get(it) })
